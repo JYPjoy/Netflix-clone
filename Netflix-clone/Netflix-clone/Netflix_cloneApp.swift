ddea87ae7d203ad1ea265dbad4162e302e48d707
JYPjoy <jypjoy@gmail.com>
2022-07-18 18:14:03 +0900

    :tada: 새 프로젝트 생성

---
+++ Netflix-clone/Netflix-clone/Netflix_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_cloneApp.swift
+//  Netflix-clone
+//
+//  Created by Jiyoung Park on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
