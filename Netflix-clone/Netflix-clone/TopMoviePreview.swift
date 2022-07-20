616a7e552a7e06b1e2629778ad48fe12723b0299
JYPjoy <jypjoy@gmail.com>
2022-07-20 16:50:57 +0900

    ✨TopMoviePreview 추가

---
+++ Netflix-clone/Netflix-clone/TopMoviePreview.swift
@@ -0,0 +1,62 @@
+//
+//  TopMoviePreview.swift
+//  Netflix-clone
+//
+//  Created by Jiyoung Park on 2022/07/20.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct TopMoviePreview: View {
+    var movie: Movie
+    
+    func isCategoryLast(_ cat: String) -> Bool {
+        let catCount = movie.categories.count
+        
+        if let index = movie.categories.firstIndex(of: cat){
+            if index + 1 != catCount {
+                return false
+            }
+        }
+        return true
+    }
+    
+    
+    var body: some View {
+        ZStack{
+            //image와 3개 modifier 세트로 알아두기(resizable, scaledtofill, clipper)
+            KFImage(movie.thumbnailURL)
+                .resizable()
+                .scaledToFill()
+                .clipped()
+            
+            VStack{
+                Spacer()
+                HStack{
+                    ForEach(movie.categories, id: \.self) { category in
+                        
+                        HStack {
+                            Text(category)
+                            
+                            if !isCategoryLast(category) {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.blue)
+                                    .font(.system(size: 3))
+                            } 
+  
+                        }
+                    }
+                }
+                Text("Row of buttons")
+            }
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct TopMoviePreview_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviePreview(movie: exampleMovie1)
+    }
+}
